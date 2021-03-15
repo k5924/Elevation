2417d226e47d4f2cd90a067226bccf3224f57a85
Kamran <56407135+k5924@users.noreply.github.com>
2021-03-15 15:37:05 +0000

    Created DevelopedSearcher for task 2

---
+++ src/adp/elevation/ui/DevelopedSearcher.java
@@ -0,0 +1,20 @@
+package adp.elevation.ui;
+
+import java.awt.image.BufferedImage;
+
+import adp.elevation.jar.AbstractSearcher;
+
+public class DevelopedSearcher extends AbstractSearcher {
+
+	public DevelopedSearcher(BufferedImage raster, int side, double deviationThreshold) {
+		super(raster, side, deviationThreshold);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
